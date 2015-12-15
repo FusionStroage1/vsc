6fd789b6578909d8018f8f153ac14068b9faaacd
kelonzhu <514841676@qq.com>
2015-12-15 23:31:57 +0800

    Create vsc_ioctl.h

---
+++ vsc_ioctl.h
@@ -0,0 +1,16 @@
+/********************************************************************
+            Copyright (C) Huawei Technologies, 2012
+  #描述:  
+
+********************************************************************/
+#include "vsc_common.h"
+
+#ifndef __VSC_IOCTL_H_
+#define __VSC_IOCTL_H_
+
+
+int __init vsc_ioctl_init(void);
+
+void vsc_ioctl_exit(void);
+
+#endif
